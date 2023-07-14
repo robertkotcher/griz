40795287dc1af6a2efe52fe12d9345855176076d
Robert Kotcher <rkotcher+1@gmail.com>
2023-07-14 13:05:35 -0400

    everything working, prior to images

--- main.py
+++ main.py
@@ -3,21 +3,15 @@ import os
 from flask import Flask, request, render_template
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='static', template_folder='static')
-CORS(app)
-
-
-#--------------------------------------------------------------------------------------------------------
-
-
-
 from langchain.agents import initialize_agent, create_pandas_dataframe_agent
 from langchain.agents import Tool
 from langchain.llms import OpenAI
 from langchain.tools import BaseTool
 from langchain.evaluation.loading import load_dataset
-from langchain import LLMMathChain
+from langchain.chains import LLMChain
+from langchain import PromptTemplate
 import numpy as np
+import subprocess
 
 from sklearn.linear_model import LinearRegression
 
@@ -27,7 +21,13 @@ import requests
 import pandas as pd
 import os
 import re
-import json
+
+app = Flask(__name__, static_folder='static', template_folder='static')
+CORS(app)
+
+
+#--------------------------------------------------------------------------------------------------------
+
 
 
 OPENAI_API_KEY = "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"
@@ -44,8 +44,9 @@ embed_model = "text-embedding-ada-002"
 pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENV)
 openai.api_key = OPENAI_API_KEY
 
-llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+llm = OpenAI(openai_api_key=OPENAI_API_KEY, model_name='gpt-4')
 
+# credit: griztech
 # BigQuery has atypical syntax which can be kind of annoying. This is a
 # convenience tool that allows you to write queries as expected.
 def applyBigQueryProjectIdHack(queryText):
@@ -71,13 +72,12 @@ def applyBigQueryProjectIdHack(queryText):
             # modify. If it's expressed without a schema, then it's
             # almost certainly from a named subquery in the text.
             if dotCount == 1:
-                #modified_table_text = f'`bigquery-public-data.{table.replace("`", "")}`'
-                modified_table_text = '`bigquery-public-data.{}{}`'.format(table.replace("`", ""), '`')
+                modified_table_text = f'`bigquery-public-data.{table.replace("`", "")}`'
                 splitText[tableIdx] = modified_table_text
 
     return ''.join(splitText)
 
-
+# credit: griztech
 def queryResponseToDataFrame(sqlQuery):
     reqBody = {
         "queryText": applyBigQueryProjectIdHack(sqlQuery)
@@ -85,9 +85,10 @@ def queryResponseToDataFrame(sqlQuery):
     resp = requests.post(QUERY_URL + "query", json=reqBody).json()
     cols = [col["name"] for col in resp["columns"]]
     rows = resp["rows"]
-    return pd.DataFrame(rows, columns=cols)
+    return pd.DataFrame(rows, columns=cols).to_json()
 
-def generateQueryWithEmbedding(query):
+
+def get_embedding_text_from_query(query):
     res = openai.Embedding.create(
         input=[query],
         engine=embed_model
@@ -97,64 +98,141 @@ def generateQueryWithEmbedding(query):
     res = pinecone_index.query(pinecone_query, top_k=2, include_metadata=True)
 
     record_txt = res["matches"][0]["metadata"]["text"]
-
-    # # generate a query to send to openai
-    # query += "---\n\nHere are some relevant tables. Please choose the most relevant one and query it:\n\n"
-    # query += record_txt
-    # query += "\n\n"
-    # query += "An important rule is that if output is SQL, table must be qualified with dataset_id. so <dataset_id>.<table>\n"
-    # query += "\nThought: only run a sql query if user asks you to execute the query.\n"
     return record_txt
 
+def get_answer_from_schema(query, schema):
+    prompt = PromptTemplate(
+        input_variables=["query", "schema"],
+        template="""
+        Task: Do you know the answer from just the information provided? If so, please answer. Otherwise, say FALSE (all caps)
+
+        Schema information:
+        {schema}
+
+        Query: {query}
+        Answer:
+        """,
+    )
+    chain = LLMChain(llm=llm, prompt=prompt)
+    response = chain.run({ "query": query, "schema": schema })
+    return response
+
+def get_sql_from_schema(query, schema):
+    prompt = PromptTemplate(
+        input_variables=["query", "schema"],
+        template="""
+        Objective:
+        - If this query requires us to look at a dataset, generate SQL to fetch the data. Please wrap the SQL query with ```sql, and ```
+        - If this query does not require any further information, output FALSE (all caps)
+
+        Detail: You must provide the table name in format <dataset>.<tablename>
+
+        Schema information:
+        {schema}
+
+        Query: {query}
+        Answer:
+        """,
+    )
+    chain = LLMChain(llm=llm, prompt=prompt)
+    response = chain.run({ "query": query, "schema": schema })
+    print(f"""
+        Thought: do we need to query the data?
+                {response}
+    """)
+    return response
+
+def get_python_from_schema_and_data(query, schema, table_data):
+    prompt = PromptTemplate(
+        input_variables=["query", "schema", "table_data"],
+        template="""
+        Task: We want to generate Python code to answer the following query
+        Query: {query}
+
+        Information we have:
+
+        Schema:
+        {schema}
+
+        Query results as data frame:
+        {table_data}
+
+        Write a Python script that, when executed, prints the answer to the query to stdout.
+        Please wrap the code with ```python, and ```
+
+        Python code:
+        """,
+    )
+    chain = LLMChain(llm=llm, prompt=prompt)
+    response = chain.run({ "query": query, "schema": schema, "table_data": table_data })
+    return response
 
 
-def askQuestionAboutData(params):
-    print("asking further questions about the data!")
-    print(params)
-    return "none"
+def extract_code(input_string, lang):
+    pattern = r"```LANG\n([\s\S]+?)\n```"
+    pattern = pattern.replace("LANG", lang)
+    match = re.search(pattern, input_string)
+    if match:
+        return match.group(1)
+    else:
+        return None
 
-generateQueryWithEmbeddingTool = Tool(
-    name="generateQueryWithEmbedding",
-    func=generateQueryWithEmbedding,
-    description="useful when to answer questions about datasets or tables."
-)
-sqlTool = Tool.from_function(
-    name="sqlTool",
-    func=queryResponseToDataFrame,
-    # docs recommend that for functions with multiple parameters to connect with some separating token
-    description="useful when user explicitly asks you to execute a sql query. Input consists of the SQL query to run.",
-)
-askQuestionAboutDataTool = Tool.from_function(
-    name="askQuestionAboutDataTool",
-    func=askQuestionAboutData,
-    # docs recommend that for functions with multiple parameters to connect with some separating token
-    description="useful when asking questions about table data returned from executing a SQL query. The dataframe and original prompt should be passed as a string joined by a semi-colon:   <dataframe>:<prompt>",
-)
-
-tools = [generateQueryWithEmbeddingTool]
-
-
-agent = initialize_agent(
-    tools,
-    llm,
-    agent="zero-shot-react-description",
-    verbose=True
-)
-
-
+def execute_code_with_output(code_string):
+    # Run the code as a subprocess
+    process = subprocess.Popen(['python', '-c', code_string],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               universal_newlines=True)
+    
+    # Capture the stdout and stderr
+    stdout, stderr = process.communicate()
+    
+    # Return the stdout
+    return stdout
 
 
 #--------------------------------------------------------------------------------------------------------
 
+def construct_response(msg, status):
+    return {
+        'data': msg,
+        'status': status
+    }
+
 @app.route("/api/query")
 def handle_query():
     query = request.args.get('q')
-    res = agent.run(query)
-    response = {
-        'data': res,
-        'status': 200,
-    }
-    return response
+    print(f"Observation: Got query: {query}")
+    
+    schema = get_embedding_text_from_query(query)
+    print(f"Observation: Found this schema: {schema}")
+
+    # we can answer without seeing anything besides the schema
+    a = get_answer_from_schema(query, schema)
+    print(f"Thought: Can we answer the question? {a}")
+    if a != "FALSE":
+        return construct_response(a, 200)
+
+    # we need data, and maybe more things
+    sql = get_sql_from_schema(query, schema)
+    if sql != "FALSE":
+        # first, fetch the data
+        sql = extract_code(sql, "sql")
+        print(f"Plan: about to execute:")
+        print(sql)
+        table_data = queryResponseToDataFrame(sql)
+
+        # then we can ask a question about it. Maybe the query itself was enough, in
+        # which case the python program will be trivial
+        script = get_python_from_schema_and_data(query, schema, table_data)
+        script = extract_code(script, "python")
+        print(f"Plan: about to execute:")
+        print(script)
+        result = execute_code_with_output(script)
+
+        return construct_response(result, 200)
+    
+    return construct_response("I could not determine the answer to your question!", 200)
 
 @app.route("/")
 def hello_world():
